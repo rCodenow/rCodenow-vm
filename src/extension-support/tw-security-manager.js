273d85a6f9870f8e72e05fc2fcd3afbed139665e
0832k12 <0832k12@gmail.com>
2023-07-16 17:06:40 +0800

    ⚡上传vm!

---
+++ src/extension-support/tw-security-manager.js
@@ -0,0 +1,143 @@
+/* eslint-disable no-unused-vars */
+
+/**
+ * Responsible for determining various policies related to custom extension security.
+ * The default implementation prevents automatic extension loading, but grants any
+ * loaded extensions the maximum possible capabilities so as to retain compatibility
+ * with a vanilla scratch-vm. You may override properties of an instance of this class
+ * to customize the security policies as you see fit, for example:
+ * ```js
+ * vm.securityManager.getSandboxMode = (url) => {
+ *   if (url.startsWith("https://example.com/")) {
+ *     return "unsandboxed";
+ *   }
+ *   return "iframe";
+ * };
+ * vm.securityManager.canAutomaticallyLoadExtension = (url) => {
+ *   return confirm("Automatically load extension: " + url);
+ * };
+ * vm.securityManager.canFetch = (url) => {
+ *   return url.startsWith('https://turbowarp.org/');
+ * };
+ * vm.securityManager.canOpenWindow = (url) => {
+ *   return url.startsWith('https://turbowarp.org/');
+ * };
+ * vm.securityManager.canRedirect = (url) => {
+ *   return url.startsWith('https://turbowarp.org/');
+ * };
+ * ```
+ */
+class SecurityManager {
+    /**
+     * Determine the typeof sandbox to use for a certain custom extension.
+     * @param {string} extensionURL The URL of the custom extension.
+     * @returns {'worker'|'iframe'|'unsandboxed'|Promise<'worker'|'iframe'|'unsandboxed'>}
+     */
+    getSandboxMode (extensionURL) {
+        // Default to worker for Scratch compatibility
+        return Promise.resolve('worker');
+    }
+
+    /**
+     * Determine whether a custom extension that was stored inside a project may be
+     * loaded. You could, for example, ask the user to confirm loading an extension
+     * before resolving.
+     * @param {string} extensionURL The URL of the custom extension.
+     * @returns {Promise<boolean>|boolean}
+     */
+    canLoadExtensionFromProject (extensionURL) {
+        // Default to false for security
+        return Promise.resolve(true);
+    }
+
+    /**
+     * Determine whether an extension is allowed to fetch a remote resource URL.
+     * This only applies to unsandboxed extensions that use the appropriate Scratch.* APIs.
+     * Sandboxed extensions ignore this entirely as there is no way to force them to use our APIs.
+     * data: and blob: URLs are always allowed (this method is never called).
+     * @param {string} resourceURL
+     * @returns {Promise<boolean>|boolean}
+     */
+    canFetch (resourceURL) {
+        // By default, allow any requests.
+        return Promise.resolve(true);
+    }
+
+    /**
+     * Determine whether an extension is allowed to open a new window or tab to a given URL.
+     * This only applies to unsandboxed extensions. Sandboxed extensions are unable to open windows.
+     * javascript: URLs are always rejected (this method is never called).
+     * @param {string} websiteURL
+     * @returns {Promise<boolean>|boolean}
+     */
+    canOpenWindow (websiteURL) {
+        // By default, allow all.
+        return Promise.resolve(true);
+    }
+
+    /**
+     * Determine whether an extension is allowed to redirect the current tab to a given URL.
+     * This only applies to unsandboxed extensions. Sandboxed extensions are unable to redirect the parent
+     * window, but are free to redirect their own sandboxed window.
+     * javascript: URLs are always rejected (this method is never called).
+     * @param {string} websiteURL
+     * @returns {Promise<boolean>|boolean}
+     */
+    canRedirect (websiteURL) {
+        // By default, allow all.
+        return Promise.resolve(true);
+    }
+
+    /**
+     * Determine whether an extension is allowed to record audio from the user's microphone.
+     * This could include raw audio data or a transcriptions.
+     * Note that, even if this returns true, success is not guaranteed.
+     * @returns {Promise<boolean>|boolean}
+     */
+    canRecordAudio () {
+        return Promise.resolve(true);
+    }
+
+    /**
+     * Determine whether an extension is allowed to record video from the user's camera.
+     * Note that, even if this returns true, success is not guaranteed.
+     * @returns {Promise<boolean>|boolean}
+     */
+    canRecordVideo () {
+        return Promise.resolve(true);
+    }
+
+    /**
+     * Determine whether an extension is allowed to read values from the user's clipboard
+     * without user interaction.
+     * Note that, even if this returns true, success is not guaranteed.
+     * @returns {Promise<boolean>|boolean}
+     */
+    canReadClipboard () {
+        return Promise.resolve(true);
+    }
+
+    /**
+     * Determine whether an extension is allowed to show notifications.
+     * Note that, even if this returns true, success is not guaranteed.
+     * @returns {Promise<boolean>|boolean}
+     */
+    canNotify () {
+        return Promise.resolve(true);
+    }
+    /**
+     * rCodenow do.
+     */
+    isCcx (extensionURL){
+        if (extensionURL.indexOf("data:application/x-zip-compressed;base64,") !== -1 || extensionURL.indexOf("data:application/x-zip-compressed;base64,") !== -1){
+            console.log('isccx')
+            return true;
+        }
+        else{
+            console.log('it isn\'t ccx')
+            return false;
+        }
+    }
+}
+
+module.exports = SecurityManager;
